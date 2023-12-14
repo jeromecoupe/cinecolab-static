ed1947560b35ef0c59978463fa65b744d35a75f8
Jérôme Coupé <jercoupe@gmail.com>
2023-12-14 16:26:46 +0100

    nav and blog section

---
+++ src/_data/mainnav.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    navLabel: "Ressources",
+    navTrigger: "resources",
+    navUrl: "/ressources/",
+  },
+  {
+    navLabel: "Blog",
+    navTrigger: "blog",
+    navUrl: "/blog/",
+  },
+  {
+    navLabel: "Formations",
+    navTrigger: "trainings",
+    navUrl: "/formations/",
+  },
+  {
+    navLabel: "A propos",
+    navTrigger: "about",
+    navUrl: "/a-propos/",
+  },
+];
